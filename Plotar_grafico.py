fdbd809fe32ad7ddeb3e4bc06913477cbf0e5fb4
Eduardo Magno <87732656+edddumgn@users.noreply.github.com>
2022-11-16 10:05:11 -0300

    Add files via upload

---
+++ Plotar_grafico.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+valores = [432, 344, 544, 546, 876, 435, 564, 432, 645, 356, 765, 843]
+
+matplotlib.pyplot.plot(meses,valores)
+matplotlib.pyplot.show()
+
